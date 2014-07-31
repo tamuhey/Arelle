ba48ebfcc75e3809817d30c924bc1f4184e3034b
Jay Leinbach <jason.leinbach@webfilings.com>
2014-07-31 10:10:27 -0600

    Bug in our patch to arelle caught in testing.  Missing 'self.'.  Refs XS-1668

--- arelle/ModelXbrl.py
+++ arelle/ModelXbrl.py
@@ -952,7 +952,7 @@ class ModelXbrl:
                                 ref["href"] = file + "#" + XmlUtil.elementFragmentIdentifier(arg)
                                 ref["sourceLine"] = arg.sourceline
                                 ref["objectId"] = arg.objectId()
-                                if logRefObjectProperties:
+                                if self.logRefObjectProperties:
                                     try:
                                         ref["properties"] = propValues(arg.propertyView)
                                     except AttributeError:
