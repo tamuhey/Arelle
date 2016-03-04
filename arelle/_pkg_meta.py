ae475595a52f4a52478d0be8cad33a8db0607b74
Andrew Perkins <andrew.perkins@workiva.com>
2016-03-04 10:14:38 -0700

    updated version number
    re: XS-2856

--- arelle/_pkg_meta.py
+++ arelle/_pkg_meta.py
@@ -1,5 +1,5 @@
 """
 Package version information module.
 """
-version_info = (0, 9, 10) # Major, Minor, Fix
+version_info = (0, 9, 13) # Major, Minor, Fix
 version = ".".join(map(str, version_info))
