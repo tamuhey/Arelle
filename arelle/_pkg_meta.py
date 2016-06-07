ee4a80432cd461c524c80f39880b9307d823662a
Jay Leinbach <jason.leinbach@webfilings.com>
2016-06-06 18:13:15 -0600

    Trying again.

--- arelle/_pkg_meta.py
+++ arelle/_pkg_meta.py
@@ -1,5 +1,5 @@
 """
 Package version information module.
 """
-version_info = (0, 9, 22, 'rc5') # Major, Minor, Fix
+version_info = (0, 9, 22, 'rc6') # Major, Minor, Fix
 version = ".".join(map(str, version_info))
