6983ad43613c369362e56a6b549f30ed26c3e0dd
jasonleinbach-wf <jason.leinbach@webfilings.com>
2016-11-09 13:18:17 -0700

    Add a rc tag.

--- arelle/_pkg_meta.py
+++ arelle/_pkg_meta.py
@@ -1,5 +1,5 @@
 """
 Package version information module.
 """
-version_info = (0, 9, 28) # Major, Minor, Fix
+version_info = (0, 9, 29, 'rc1') # Major, Minor, Fix
 version = ".".join(map(str, version_info))
