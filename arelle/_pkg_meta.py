9c88a721296bebc3bde8ad6ab1c6ef4f3b4d07ef
Brad Benjamin <brad.benjamin@webfilings.com>
2016-11-10 10:32:19 -0700

    refined logging with more output to see which items take long.

--- arelle/_pkg_meta.py
+++ arelle/_pkg_meta.py
@@ -1,5 +1,5 @@
 """
 Package version information module.
 """
-version_info = (0, 9, 29, 'rc3') # Major, Minor, Fix
+version_info = (0, 9, 29, 'rc4') # Major, Minor, Fix
 version = ".".join(map(str, version_info))
