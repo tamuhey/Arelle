d405a2deb766d0c197ba7eddc372164edc560ad3
jasonleinbach-wf <jason.leinbach@webfilings.com>
2016-12-12 10:22:39 -0700

    Version bump.  Refs XS-3936

--- arelle/_pkg_meta.py
+++ arelle/_pkg_meta.py
@@ -1,5 +1,5 @@
 """
 Package version information module.
 """
-version_info = (0, 9, 31) # Major, Minor, Fix
+version_info = (0, 9, 32) # Major, Minor, Fix
 version = ".".join(map(str, version_info))
